cc00c06dc2c8fe636e0a4835ad57841400ce02d1
apeal2real <alec.peal@gmail.com>
2024-02-26 10:36:35 -0700

    Add project files.

---
+++ Mission8_Grp1_6/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Mission8_Grp1_6.Models;
+using System.Diagnostics;
+
+namespace Mission8_Grp1_6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission8_Grp1_6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission8_Grp1_6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
