9dab125976c9662cf9b2e84cec30c787db4915b7
ElainaHalls <130600410+ElainaHalls@users.noreply.github.com>
2024-02-29 10:49:08 -0700

    added builder.services for teh ITaskRepo and EFTaskRepo files.

--- Mission8_Grp1_6/Program.cs
+++ Mission8_Grp1_6/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<TaskContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("TaskConnection"));
 });
 
+builder.Services.AddScoped<ITaskRepository, EFTaskRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
