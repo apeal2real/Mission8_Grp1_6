3bbb6f87c363ac37095eff18d99311f68f2eb6a8
ElainaHalls <130600410+ElainaHalls@users.noreply.github.com>
2024-02-28 18:20:32 -0700

    Updates to the Models folder.

--- Mission8_Grp1_6/Models/Task.cs
+++ Mission8_Grp1_6/Models/Task.cs
@@ -6,13 +6,15 @@ namespace Mission8_Grp1_6.Models
     public class Task
     {
         [Key]
-        
+        [Required]
         public int TaskID { get; set; }
 
+        [Required]
         public string TaskName { get; set; }
 
         public DateTime? DueDate { get; set; }
 
+        [Required]
         public int Quadrant { get; set; }
 
         public string? Category { get; set; }
--- Mission8_Grp1_6/Models/TaskContext.cs
+++ Mission8_Grp1_6/Models/TaskContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
 
 namespace Mission8_Grp1_6.Models
 {
@@ -13,11 +14,11 @@ namespace Mission8_Grp1_6.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Task>().HasData(
-                new Task { TaskID = 1, Category = "Home" },
-                new Task { TaskID = 2, Category = "School" },
-                new Task { TaskID = 3, Category = "Work" },
-                new Task { TaskID = 4, Category = "Church" }
-               );
+                 new Task { TaskID = 1, TaskName = "Task 1 Name", Quadrant = 1, Category = "Home", Completed = false },
+                 new Task { TaskID = 2, TaskName = "Task 2 Name", Quadrant = 2, Category = "School", Completed = false },
+                 new Task { TaskID = 3, TaskName = "Task 3 Name", Quadrant = 3, Category = "Work", Completed = false },
+                 new Task { TaskID = 4, TaskName = "Task 4 Name", Quadrant = 4, Category = "Church", Completed = false }
+            );
         }
     }
 }
