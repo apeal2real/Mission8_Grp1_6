6108ec87c04b5758ea9756b0374a6a60f5d81ac0
ElainaHalls <130600410+ElainaHalls@users.noreply.github.com>
2024-02-28 17:13:48 -0700

    Database Creation

---
+++ Mission8_Grp1_6/Models/Task.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mission8_Grp1_6.Models
+{
+    public class Task
+    {
+        [Key]
+        [Required]
+        public int TaskID { get; set; }
+
+        [Required]
+        public string TaskName { get; set; }
+        public DateTime? DueDate { get; set; }
+
+        [Required]
+        public int Quadrant { get; set; }
+
+        public string? Category { get; set; }
+
+        public bool? Completed { get; set; }
+    }
+}
---
+++ Mission8_Grp1_6/Models/TaskContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission8_Grp1_6.Models
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions<TaskContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Task>().HasData(
+                new Task { TaskID = 1, Category = "Home" },
+                new Task { TaskID = 2, Category = "School" },
+                new Task { TaskID = 3, Category = "Work" },
+                new Task { TaskID = 4, Category = "Church" }
+               );
+        }
+    }
+}
