7aff8c09ea4e9dd73fc1b7bc109da49e95eec3b9
christensen-dev <dogsippepsi@gmail.com>
2024-03-01 13:06:00 -0700

    Commit

--- Mission8_Grp1_6/Controllers/HomeController.cs
+++ Mission8_Grp1_6/Controllers/HomeController.cs
@@ -7,11 +7,11 @@ namespace Mission8_Grp1_6.Controllers
 
     public class HomeController : Controller
     {
-        private TaskContext _context;
+        private ITaskRepository _repo;
 
-        public HomeController(TaskContext temp)
+        public HomeController(ITaskRepository temp)
         {
-            _context = temp;
+            _repo = temp;
         }
 
         // [HttpGet]
@@ -33,8 +33,8 @@ namespace Mission8_Grp1_6.Controllers
         [HttpGet]
         public IActionResult AddEdit()
         {
-            ViewBag.Tasks = _context.Tasks.ToList();
-            ViewBag.Categories = _context.Categories
+            ViewBag.Tasks = _repo.Tasks.ToList();
+            ViewBag.Categories = _repo.Categories
                 .OrderBy(x => x.CategoryID)
                 .ToList();
             return View(new Mission8_Grp1_6.Models.Task());
@@ -43,19 +43,18 @@ namespace Mission8_Grp1_6.Controllers
         [HttpPost]
         public IActionResult AddEdit(Mission8_Grp1_6.Models.Task response) //form -> AddEdit
         {
-            ViewBag.Tasks = _context.Tasks.ToList();
-            _context.Tasks.Add(response); //add record to database
-            _context.SaveChanges();
+            ViewBag.Tasks = _repo.Tasks.ToList();
+            _repo.AddTask(response); //add record to database
             return RedirectToAction("Index");
         }
 
         public IActionResult Index(Mission8_Grp1_6.Models.Task response)
         {
-            ViewBag.Tasks = _context.Tasks.ToList();
-            var tasks = _context.Tasks
+            ViewBag.Tasks = _repo.Tasks.ToList();
+            var tasks = _repo.Tasks
                 .ToList();
 
-            ViewBag.Categories = _context.Categories
+            ViewBag.Categories = _repo.Categories
                 .OrderBy(x => x.CategoryID)
                 .ToList();
 
@@ -65,29 +64,28 @@ namespace Mission8_Grp1_6.Controllers
         [HttpGet]
         public IActionResult Edit(int id)
         {
-            var recordToEdit = _context.Tasks
+            var recordToEdit = _repo.Tasks
                 .Single(x => x.TaskID == id);
-            ViewBag.Categories = _context.Categories
+            ViewBag.Categories = _repo.Categories
                 .OrderBy(x => x.CategoryID)
                 .ToList();
 
-            ViewBag.Tasks = _context.Tasks.ToList();
+            ViewBag.Tasks = _repo.Tasks.ToList();
             return View("AddEdit", recordToEdit); //addedit
         }
 
         [HttpPost]
         public IActionResult Edit(Mission8_Grp1_6.Models.Task updated)
         {
-            _context.Update(updated);
-            _context.SaveChanges();
+            _repo.UpdateTask(updated);
             return RedirectToAction("Index");
         }
 
         [HttpGet]
         public IActionResult Delete(int id)
         {
-            ViewBag.Tasks = _context.Tasks.ToList();
-            var recordToDelete = _context.Tasks
+            ViewBag.Tasks = _repo.Tasks.ToList();
+            var recordToDelete = _repo.Tasks
                 .Single(x => x.TaskID == id);
 
             return View("Confirmation", recordToDelete);
@@ -96,9 +94,13 @@ namespace Mission8_Grp1_6.Controllers
         [HttpPost]
         public IActionResult Delete(Mission8_Grp1_6.Models.Task deleted)
         {
-            _context.Remove(deleted);
-            _context.SaveChanges();
-
+            _repo.RemoveTask(deleted);
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult Check(Mission8_Grp1_6.Models.Task task)
+        {
+            task.Completed = true;
             return RedirectToAction("Index");
         }
     }
--- Mission8_Grp1_6/Models/ICategoryRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Mission8_Grp1_6.Models
-{
-    public class ICategoryRepository
-    {
-        List<Category>? Categories { get; }
-    }
-}
